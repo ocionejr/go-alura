0507d7a6fd537d68936bba090cb1cb10cf3bd6ab
ocionejr <ocionejunior@gmail.com>
2021-11-03 10:43:32 -0300

    feat: reading command

---
+++ hello.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	exibeIntroducao()
+	exibeMenu()
+
+	// if comando == 1 {
+	// 	fmt.Println("Monitorando...")
+	// } else if comando == 2 {
+	// 	fmt.Println("Exibindo logs...")
+	// } else if comando == 0 {
+	// 	fmt.Println("Saindo do programa")
+	// } else {
+	// 	fmt.Println("Não conheço este comando")
+	// }
+
+	switch leComando() {
+	case 1:
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println("Exibindo logs...")
+	case 0:
+		fmt.Println("Saindo do programa")
+		os.Exit(0)
+	default:
+		fmt.Println("Não conheço este comando")
+		os.Exit(-1)
+	}
+}
+
+func exibeIntroducao() {
+	nome := "Ocione"
+	versao := 1.16
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
+}
+
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi", comandoLido)
+	return comandoLido
+}
+
+func exibeMenu() {
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+}
