c638989c36804191343783e04de4af4f84928493
ocionejr <ocionejunior@gmail.com>
2021-11-03 14:59:29 -0300

    feat: print logs in txt

--- hello.go
+++ hello.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -80,8 +81,10 @@ func testaSite(site string) {
 	} else {
 		if resp.StatusCode == 200 {
 			fmt.Println("Site:", site, "foi carregado com sucesso!")
+			registraLog(site, true)
 		} else {
 			fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+			registraLog(site, false)
 		}
 	}
 }
@@ -112,4 +115,16 @@ func leSitesDoArquivo() []string {
 
 	return sites
 
+}
+
+func registraLog(site string, status bool) {
+	arquivo, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + "- online: " + strconv.FormatBool(status) + "\n")
+
+	arquivo.Close()
 }
